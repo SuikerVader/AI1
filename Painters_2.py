c193dbb4cff9aaf7469ab7f8bc086bffbee46460
jesse <jessevanloock@gmail.com>
2019-11-30 19:18:33 +0100

    Mooier script

--- Painters_2.py
+++ Painters_2.py
@@ -12,64 +12,69 @@ import cv2
 from PIL import Image
 import glob
 
+training_image_list = []
+training_label_list = []
+validation_image_list = []
+validation_label_list = []
+test_image_list = []
+history = None
 
-def main():
+imageSize = 150
+totalImages = 470
+training_percent = 0.7
+validation_percent = 0.15
+test_percent = 0.15
+
+test_painting1 = 0
+test_painting2 = 80
+
+painters = ["RubensAll", "PicassoAll"]
+
+model = models.Sequential()
+
+def plot_graphs():
+    global history
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    epochs = range(1, len(acc) + 1)
+    plt.plot(epochs, acc, 'bo', label='Training acc')
+    plt.plot(epochs, val_acc, 'b', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.legend()
+    plt.figure()
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+    plt.show()
+
+def fillArray(painter: painters, i: int):
     print('-----------------------------------------------------------------')
     print('\nGetting painting images from folders.\n')
+    counter = 0
+    for filename in glob.glob('Paintings/' + painter + '/*'):
+        im = cv2.imread(filename)
+        if im is None:
+            print("Couldn't open file %s" % filename)
+        else:
+            if counter < totalImages * training_percent:
+                training_image_list.append(cv2.resize(im, (imageSize, imageSize), interpolation=cv2.INTER_CUBIC))
+                training_label_list.append(i)
+                counter += 1
+            elif counter < totalImages * (training_percent + validation_percent):
+                validation_image_list.append(cv2.resize(im, (imageSize, imageSize), interpolation=cv2.INTER_CUBIC))
+                validation_label_list.append(i)
+                counter += 1
+            elif counter < totalImages * (training_percent + validation_percent + test_percent):
+                test_image_list.append(cv2.resize(im, (imageSize, imageSize), interpolation=cv2.INTER_CUBIC))
 
-    training_image_list = []
-    training_label_list = []
-    validation_image_list = []
-    validation_label_list = []
-    test_image_list = []
 
-    imageSize = 150
-    painters = ["RubensAll", "PicassoAll"]
-
-    def fillArray(painter: painters, i: int):
-        counter = 0
-        for filename in glob.glob('Paintings/' + painter + '/*'):
-            im = cv2.imread(filename)
-            if im is None:
-                print("Couldn't open file %s" % filename)
-            else:
-                if counter < 350:
-                    training_image_list.append(cv2.resize(im, (imageSize, imageSize), interpolation=cv2.INTER_CUBIC))
-                    training_label_list.append(i)
-                    counter += 1
-                elif counter < 410:
-                    validation_image_list.append(cv2.resize(im, (imageSize, imageSize), interpolation=cv2.INTER_CUBIC))
-                    validation_label_list.append(i)
-                    counter += 1
-                elif counter < 470:
-                    test_image_list.append(cv2.resize(im, (imageSize, imageSize), interpolation=cv2.INTER_CUBIC))
-
-    for i in range(len(painters)):
-        fillArray(painters[i], i)
-
-    instance1 = test_image_list[0]
-    instance2 = test_image_list[72]
-
-    print('-----------------------------------------------------------------')
-    print('\nShow Rubens Painting.\n')
-
-    imgplot = plt.imshow(training_image_list[0])
-    plt.show()
-
-    print('-----------------------------------------------------------------')
-    print('\nShow Picasso Painting.\n')
-
-    imgplot = plt.imshow(training_image_list[500])
-    plt.show()
-
+def create_model():
     print('\n==================================================================')
     print('MAKING THE MODEL.')
 
-    training_image_list = np.array((training_image_list))
-    validation_image_list = np.array(validation_image_list)
-    test_image_list = np.array(test_image_list)
-
-    model = models.Sequential()
     model.add(layers.Conv2D(16, (3, 3), activation='relu', input_shape=(imageSize, imageSize, 3)))
     model.add(layers.MaxPooling2D((2, 2)))
     model.add(layers.Conv2D(16, (3, 3), activation='relu'))
@@ -83,14 +88,18 @@ def main():
     model.add(layers.Dense(512, activation='relu'))
     model.add(layers.Dense(1, activation='sigmoid'))
 
+def compile_model():
     print('COMPILING THE MODEL.')
-    model.compile(loss='binary_crossentropy', optimizer=optimizers.RMSprop(lr=1e-5), metrics=['acc'])
+    model.compile(loss='binary_crossentropy', optimizer=optimizers.RMSprop(lr=1e-4), metrics=['acc'])
 
+def train_model():
     print('-----------------------------------------------------------------')
     print('\nTraining the network.\n')
 
     batch_size = 25
-    epochs = 50
+    epochs = 250
+
+    global history
 
     history = model.fit(training_image_list,
                         training_label_list,
@@ -99,7 +108,9 @@ def main():
                         verbose=2,
                         validation_data=(validation_image_list, validation_label_list))
 
-    ############# TESTING
+
+def test_model(instance1, instance2):
+	############# TESTING
     #
     # Evaluate the model on the test data.
 
@@ -120,29 +131,37 @@ def main():
     plt.imshow(instance1)
     plt.show()
 
-    print("Predicted = %s" % new_labels[0])
+    print("Predicted = %s" % new_labels[test_painting1])
 
     plt.imshow(instance2)
     plt.show()
 
-    print("Predicted = %s" % new_labels[72])
+    print("Predicted = %s" % new_labels[test_painting2])
 
-    acc = history.history['acc']
-    val_acc = history.history['val_acc']
-    loss = history.history['loss']
-    val_loss = history.history['val_loss']
-    epochs = range(1, len(acc) + 1)
-    plt.plot(epochs, acc, 'bo', label='Training acc')
-    plt.plot(epochs, val_acc, 'b', label='Validation acc')
-    plt.title('Training and validation accuracy')
-    plt.legend()
-    plt.figure()
-    plt.plot(epochs, loss, 'bo', label='Training loss')
-    plt.plot(epochs, val_loss, 'b', label='Validation loss')
-    plt.title('Training and validation loss')
-    plt.legend()
-    plt.show()
 
+
+def main():
+
+    for i in range(len(painters)):
+        fillArray(painters[i], i)
+
+    global training_image_list
+    global validation_image_list
+    global test_image_list
+
+    training_image_list = np.array((training_image_list))
+    validation_image_list = np.array(validation_image_list)
+    test_image_list = np.array(test_image_list)
+        
+    instance1 = test_image_list[test_painting1]
+    instance2 = test_image_list[test_painting2]
+
+    create_model()
+    compile_model()
+    train_model()
+    test_model(instance1, instance2)
+    plot_graphs()
+    
 
 if __name__ == "__main__":
     main()
